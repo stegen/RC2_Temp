a46dbba51336ad5be466cfe5a5a2f92456946e74
Vanessa Garayburu-Caruso <69696155+vgc40@users.noreply.github.com>
2022-03-18 09:10:16 -0700

    added other QAQC funtions

---
+++ Stats_fun_v5_only_dic.R
@@ -0,0 +1,99 @@
+Stats_fun_v5_only_dic <- function (df){
+  # This function relies on the samples to be introduced in the data as Sample_ID
+ if ("DIC_mg_C_per_L"%in% colnames(df) == TRUE){
+   # Adding a column for just sample names, no reps
+   data.stats = as.data.frame(matrix(NA,ncol = 6, nrow = nrow(df)))
+   colnames(data.stats) = c("Sample_ID","Sample_name","DIC_CV","DIC_variance","DIC_range","DIC_normalized_range")
+   data.stats$Sample_ID = df$Sample_ID
+   # Removing the replicates from the sample ID
+   data.stats$Sample_name = data.stats$Sample_ID
+   data.stats$Sample_name = gsub(pattern = "-1","",data.stats$Sample_name)
+   data.stats$Sample_name = gsub(pattern = "-2","",data.stats$Sample_name)
+   data.stats$Sample_name = gsub(pattern = "-3","",data.stats$Sample_name)
+   
+   unique.sample.names = unique(data.stats$Sample_name)
+   
+   # Calculate stats for each unique sample
+   for (i in 1:length(unique.sample.names)){
+     index = grep(unique.sample.names[i],data.stats$Sample_name)
+     DIC.temp = df$DIC_mg_C_per_L[grep(unique.sample.names[i],data.stats$Sample_name)]
+     
+     data.stats$DIC_CV[index] = sd(DIC.temp) / mean(DIC.temp) * 100
+     data.stats$DIC_range[index] =  max(DIC.temp) - min(DIC.temp)
+     data.stats$DIC_variance[index] = var(DIC.temp)
+     data.stats$DIC_normalized_range[index] = (max(DIC.temp) - min(DIC.temp))/mean(DIC.temp)
+     
+   }
+   
+   hist(data.stats$DIC_CV, breaks = 55)
+  # hist(data.stats$DIC_range, breaks = 55)
+   #hist(data.stats$DIC_variance, breaks = 55)
+   #hist(data.stats$TN_CV, breaks = 55)
+   #hist(data.stats$TN_range, breaks = 55)
+   #hist(data.stats$TN_variance, breaks = 55)
+  return(data.stats)
+ }else{
+    ions = names(df)[2:(ncol(df)-1)]
+    data.stats = as.data.frame(matrix(NA,ncol = ((length(ions)*5)+2), nrow = nrow(df)))
+    #Creating column names
+   name = c("Sample_ID","Sample_name")
+    for (i in 1:length(ions)){
+      a = paste0(ions[i],"_mg_per_L")
+      b = paste0(ions[i],"_CV")
+      c = paste0(ions[i],"_range")
+      d = paste0(ions[i],"_variance")
+      e = paste0(ions[i],"_normalized_range")
+      name = c(name,a,b,c,d,e)
+    }
+  colnames(data.stats) = name
+  data.stats$Sample_ID = df$Sample_ID
+  # Removing the replicates from the sample ID
+  data.stats$Sample_name = data.stats$Sample_ID
+  data.stats$Sample_name = gsub(pattern = "-1","",data.stats$Sample_name)
+  data.stats$Sample_name = gsub(pattern = "-2","",data.stats$Sample_name)
+  data.stats$Sample_name = gsub(pattern = "-3","",data.stats$Sample_name)
+  
+  unique.sample.names = unique(data.stats$Sample_name)
+  
+for (j in 1:length(ions)){  
+  df1 = cbind.data.frame(Sample_ID = df$Sample_ID,df[grep(pattern = ions[j],x = colnames(df))])
+  for (i in 1:length(unique.sample.names)){
+    index = grep(unique.sample.names[i],data.stats$Sample_name)
+    temp = df1[grep(unique.sample.names[i],data.stats$Sample_name),2]
+  data.stats[index,grep(pattern = paste0(ions[j],"_mg_per_L"),x = colnames(data.stats))] = temp
+    if (length(grep("TRUE",is.na(temp)))==2){
+      data.stats[index,grep(pattern = paste0(ions[j],"_CV"),x = colnames(data.stats))] = "Two-reps are NA"
+      data.stats[index,grep(pattern = paste0(ions[j],"_range"),x = colnames(data.stats))] =  "Two-reps are NA"
+      data.stats[index,grep(pattern = paste0(ions[j],"_variance"),x = colnames(data.stats))] = "Two-reps are NA"
+      data.stats[index,grep(pattern = paste0(ions[j],"_normalized_range"),x = colnames(data.stats))] =  (max(temp) - min(temp))/mean(temp)
+      
+    }else if(length(grep("TRUE",is.na(temp)))==3){
+      data.stats[index,grep(pattern = paste0(ions[j],"_CV"),x = colnames(data.stats))] = "Three-reps are NA"
+      data.stats[index,grep(pattern = paste0(ions[j],"_range"),x = colnames(data.stats))] =  "Three-reps are NA"
+      data.stats[index,grep(pattern = paste0(ions[j],"_variance"),x = colnames(data.stats))] = "Three-reps are NA"
+      data.stats[index,grep(pattern = paste0(ions[j],"_normalized_range"),x = colnames(data.stats))] =  (max(temp) - min(temp))/mean(temp)
+    }else if(length(grep("TRUE",is.na(temp)))==1){
+      data.stats[index,grep(pattern = paste0(ions[j],"_CV"),x = colnames(data.stats))] = "One-rep is NA"
+      data.stats[index,grep(pattern = paste0(ions[j],"_range"),x = colnames(data.stats))] =  "One-rep is NA"
+      data.stats[index,grep(pattern = paste0(ions[j],"_variance"),x = colnames(data.stats))] = "One-rep is NA"
+      data.stats[index,grep(pattern = paste0(ions[j],"_normalized_range"),x = colnames(data.stats))] =  (max(temp) - min(temp))/mean(temp)
+    }else{
+     data.stats[index,grep(pattern = paste0(ions[j],"_CV"),x = colnames(data.stats))] = sd(temp) / mean(temp) * 100
+    data.stats[index,grep(pattern = paste0(ions[j],"_range"),x = colnames(data.stats))] =  max(temp) - min(temp)
+    data.stats[index,grep(pattern = paste0(ions[j],"_variance"),x = colnames(data.stats))] = var(temp)
+    data.stats[index,grep(pattern = paste0(ions[j],"_normalized_range"),x = colnames(data.stats))] =  (max(temp) - min(temp))/mean(temp)
+    } 
+    
+  }
+} 
+  cv.unique = (grep("CV",colnames(data.stats)))
+  for (k in 1:length(cv.unique)){
+    if (is.numeric(data.stats[,cv.unique[k]])==TRUE){
+      hist(data.stats[,cv.unique[k]], breaks = 55, main = names(data.stats)[cv.unique[k]], xlab = names(data.stats)[cv.unique[k]])
+    }
+   
+  }
+  return(data.stats)
+  }
+
+}
